54b47426569921dffcb21612e1797bdf35343877
superman <wujian_166@163.com>
2019-10-01 10:28:05 +0800

    props：传递数据

--- src/app/index.js
+++ src/app/index.js
@@ -5,11 +5,17 @@ var uiButton = {
 
 /*全局定义组件*/
 Vue.component('ui-button-global', {
-    template: `<button class="ui button">全局定义组件</button>`
+    template: `<button class="ui button">{{text}}</button>`,
+    props: ['text']
 });
+
+var dataSource = {
+    published: false
+}
 
 var vm = new Vue({
     el: '#app',
+    data: dataSource,
     components: {
         'ui-button': uiButton
     }
