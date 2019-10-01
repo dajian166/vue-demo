eed653aeac48ce5e7f93ec0569ba2395d1d48f8c
superman <wujian_166@163.com>
2019-10-01 15:08:11 +0800

    路由

--- src/app/index.js
+++ src/app/index.js
@@ -1,8 +1,29 @@
-var dataSource = {
+const Home = {
+    template: '<h2>首页</h2>'
+}
+
+const Event = {
+    template: '<h2>活动</h2>'
+}
+
+const dataSource = {
 
 }
 
-var vm = new Vue({
+const routes = [{
+    path: '/',
+    component: Home
+}, {
+    path: '/events',
+    component: Event
+}]
+
+const router = new VueRouter({
+    routes: routes
+})
+
+const vm = new Vue({
     el: '#app',
+    router: router,
     data: dataSource
 });
